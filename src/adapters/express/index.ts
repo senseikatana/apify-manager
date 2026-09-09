a51b82c9ade930a813d4a773b62fd05c462a18b2
senseikatana - Sergio Jurado <github-senseikatana@protonmail.com>
2026-09-09 10:46:50 +0200

    feat(express): add changes to index.ts

--- src/adapters/express/index.ts
+++ src/adapters/express/index.ts
@@ -1,4 +1,10 @@
 export { app, useGetApp, useStart } from "./app.js";
-export { ProductController } from "./products.controller.js";
+export {
+	useExpressCreateProduct,
+	useExpressDeleteProduct,
+	useExpressGetAllProducts,
+	useExpressGetProductById,
+	useExpressUpdateProduct,
+} from "./products.controller.js";
 export { default as router } from "./router.js";
 export { useExpressCreate, useExpressGetApp, useExpressStart } from "./server.js";
